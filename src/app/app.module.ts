7d3d1b6c357164b970e78d5375d5be41a77687ea
Neto002 <antoniogfneto92@hotmail.com>
2022-05-04 15:40:49 -0300

    finalizando formularios de inclusão

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
 import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+
+import {CurrencyMaskModule} from 'ng2-currency-mask';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 
 registerLocaleData(localePt);
 
@@ -18,14 +23,16 @@ registerLocaleData(localePt);
   declarations: [
     AppComponent,
     MovimentacaoListComponent,
-    MovimentacaoNewComponent
+    MovimentacaoNewComponent,
+    CorrentistaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CurrencyMaskModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
